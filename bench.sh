dfc93c7571d1f130ccf808bcfb2a883d00a566c7
Ioannis Tsakpinis <iotsakp@gmail.com>
2019-08-21 16:16:43 +0300

    check configuration is correct

--- bench.sh
+++ bench.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-if [ "$(uname)" == "Darwin" ]; then
-    bash ./build-dylib.sh
+if [ -z "$JAVA_HOME" ]
+then
+    echo "Please set JAVA_HOME to the root of a JDK 8+."
 else
-    bash ./build-so.sh
+    if [ "$(uname)" == "Darwin" ]; then
+        bash ./build-dylib.sh
+    else
+        bash ./build-so.sh
+    fi
+    ./mvnw package
+    $JAVA_HOME/bin/java -Djava.library.path=target -jar target/benchmarks.jar
 fi
-./mvnw package
-$JAVA_HOME/bin/java -Djava.library.path=target -jar target/benchmarks.jar
