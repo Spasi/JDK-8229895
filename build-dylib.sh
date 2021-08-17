4f6cb1a270d2fffed8ec31061ec4dec209145193
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-17 23:28:07 +0800

    Delete build-dylib.sh

--- build-dylib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-INCLUDE_PATH="-I$JAVA_HOME/include -I$JAVA_HOME/include/darwin"
-INCLUDE_PATH="$INCLUDE_PATH -I$JAVA_HOME/../include -I$JAVA_HOME/../include/darwin"
-mkdir -p target
-cd target
-clang -c -std=c11 -m64 -O2 -fPIC -pthread -DNDEBUG $INCLUDE_PATH ../src/main/c/jni_overhead.c
-clang -dynamiclib -O2 -fPIC -o libjni_overhead.dylib *.o
-cd ..
